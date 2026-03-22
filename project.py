07e4712ced241ca9f04631df0c4b2e32a07b2d9b
lying2020 <824262075@qq.com>
2026-03-22 14:23:16 +0800

    fix: add dataset path name

--- project.py
+++ project.py
@@ -16,14 +16,28 @@ amber_data_path = "/home/liying/Documents/MLLMBenchMark/AMBER/data"
 # anole_7b_v0_1_path = "/home/liying/Documents/Anole-7b-v0.1"
 # volcano_7b_path = "/home/liying/Documents/volcano-7b"
 
-coco_data_path = "/home1/cjl/MM_2026/dataset/ms_coco"
+data_path = "/home1/cjl/MM_2026/dataset"
+coco_data_path = os.path.join(data_path, "ms_coco")
 coco_annotations_path = os.path.join(coco_data_path, "annotations")
 coco_val2014_images_path = os.path.join(coco_data_path, "val2014")
 
-llava_v15_7b_path = "/home1/cjl/models/llava-v1.5-7b"
-clip_vit_large_patch14_path = "/home1/cjl/models/clip-vit-large-patch14"
-anole_7b_v0_1_path = "/home1/cjl/models/anole-7b"
-volcano_7b_path = "/home1/cjl/models/volcano-7b"
+gqa_bench_path = os.path.join(data_path, "GQA_Bench")
+clevr_v1_0_path = os.path.join(data_path, "CLEVR_v1.0/")
+llava_instruct_150k_path = os.path.join(data_path, "LLaVA_INSTRUCT_150K/")
+MMBench_path = os.path.join(data_path, "MMBench/")
+MMBench_data_path = os.path.join(MMBench_path, "data")
+vo_cot_path = os.path.join(data_path, "vo-cot", "VoCoT")
+vsr_path = os.path.join(data_path, "VSR")
+vsr_images_path = os.path.join(vsr_path, "images")
+vsr_ramdom_path = os.path.join(vsr_path, "vsr_random")
+
+
+
+models_path = "/home1/cjl/models"
+llava_v15_7b_path = os.path.join(models_path, "llava-v1.5-7b")
+clip_vit_large_patch14_path = os.path.join(models_path, "clip-vit-large-patch14")
+anole_7b_v0_1_path = os.path.join(models_path, "anole-7b")
+volcano_7b_path = os.path.join(models_path, "volcano-7b")
 
 # 推理测试图片目录（可选）：在此目录放 default.jpg 等，run_inference 会优先用作默认图
 _project_dir = os.path.dirname(os.path.abspath(__file__))
